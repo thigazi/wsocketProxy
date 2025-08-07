220e11ffcdb40d46af79eea3d89f0127a201c027
Tamer Higazi <th80@s4us.info>
2025-08-07 21:40:30 +0200

    Application Skeleton start,stop, and PID

--- start.py
+++ start.py
@@ -1,4 +1,44 @@
-from yaml import load, dump
+from time import sleep
+from sys import argv
+from psutil import Process
+import pidfile
+from yaml import load, FullLoader
+from os.path import join
+from pathlib import Path
+
+
+class Apploader(object):
+    def __init__(self):
+        current_path = Path(__file__).parent
+        with open(join(current_path, 'config.yaml'), 'r') as fcfg:
+            self.__yaml_cfg = load(fcfg, Loader=FullLoader)
+
+    def Task(self, task_action):
+        if task_action == 'start':
+            try:
+                with pidfile.PIDFile(self.__yaml_cfg['PidFileLocation']):
+                    print('Process started.')
+                    sleep(1000)
+
+            except pidfile.AlreadyRunningError:
+                print('Process is already running.')
+
+        elif task_action == 'stop':
+            with open(self.__yaml_cfg['PidFileLocation'],'r') as pid_file:
+                pid_process = Process(int(pid_file.read()))
+                pid_process.terminate()
+
+    def __startUp(self):
+        pass
+
+    def __shutDown(self):
+        pass
+
 
 if __name__ == '__main__':
-    pass
+    print(argv)
+    if len(argv) == 2:
+        if argv[1] == 'start':
+            Apploader().Task(task_action='start')
+        if argv[1] == 'stop':
+            Apploader().Task(task_action='stop')
